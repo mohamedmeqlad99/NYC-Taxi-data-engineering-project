9cca7e0abcd1ba15b2a7e6a43a38346afaf19bf3
mohamed meqlad <mohamedmeqlad9@gmail.com>
2025-05-07 14:46:53 +0300

    Update create external file format.sql

--- ldw/create external file format.sql
+++ ldw/create external file format.sql
@@ -1,13 +1,66 @@
-USE nyc_taxi_ldw
-CREATE EXTERNAL FILE FORMAT csv_file_format
-WITH (
-    FORMAT_TYPE = DELIMITEDTEXT,
-    FORMAT_OPTIONS  (
-      FIELD_TERMINATOR = ','
-    , STRING_DELIMITER = '"'
-    , FIRST_ROW = 2 
-    , USE_TYPE_DEFAULT =  FALSE 
-    , ENCODING = 'UTF8'
-    , PARSER_VERSION = '2.0' 
-    )
-)
+USE nyc_taxi_ldw;
+
+-- Create file format csv_file_format for parser version 2.0
+IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name ='csv_file_format')
+  CREATE EXTERNAL FILE FORMAT csv_file_format  
+  WITH (  
+      FORMAT_TYPE = DELIMITEDTEXT,
+      FORMAT_OPTIONS (  
+        FIELD_TERMINATOR = ','  
+      , STRING_DELIMITER = '"'
+      , First_Row = 2
+      , USE_TYPE_DEFAULT = FALSE 
+      , Encoding = 'UTF8'
+      , PARSER_VERSION = '2.0' )   
+      );  
+
+-- Create file format csv_file_format_pv1 for parser version 1.0
+IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name ='csv_file_format_pv1')
+  CREATE EXTERNAL FILE FORMAT csv_file_format_pv1 
+  WITH (  
+      FORMAT_TYPE = DELIMITEDTEXT,
+      FORMAT_OPTIONS (  
+        FIELD_TERMINATOR = ','  
+      , STRING_DELIMITER = '"'
+      , First_Row = 2
+      , USE_TYPE_DEFAULT = FALSE 
+      , Encoding = 'UTF8'
+      , PARSER_VERSION = '1.0' )   
+      );
+
+IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name ='tsv_file_format')
+  CREATE EXTERNAL FILE FORMAT tsv_file_format
+  WITH (  
+      FORMAT_TYPE = DELIMITEDTEXT,
+      FORMAT_OPTIONS (  
+        FIELD_TERMINATOR = '\t'  
+      , STRING_DELIMITER = '"'
+      , First_Row = 2
+      , USE_TYPE_DEFAULT = FALSE 
+      , Encoding = 'UTF8'
+      , PARSER_VERSION = '2.0' )   
+      );  
+IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name ='tsv_file_format_pv1')
+  CREATE EXTERNAL FILE FORMAT tsv_file_format_pv1
+  WITH (  
+      FORMAT_TYPE = DELIMITEDTEXT,
+      FORMAT_OPTIONS (  
+        FIELD_TERMINATOR = '\t'  
+      , STRING_DELIMITER = '"'
+      , First_Row = 2
+      , USE_TYPE_DEFAULT = FALSE 
+      , Encoding = 'UTF8'
+      , PARSER_VERSION = '1.0' )   
+      );  
+
+IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name ='parquet_file_format')
+  CREATE EXTERNAL FILE FORMAT parquet_file_format  
+  WITH (  
+        FORMAT_TYPE = PARQUET,  
+        DATA_COMPRESSION = 'org.apache.hadoop.io.compress.SnappyCodec'  
+       ); 
+IF NOT EXISTS (SELECT * FROM sys.external_file_formats WHERE name ='delta_file_format')
+  CREATE EXTERNAL FILE FORMAT delta_file_format  
+  WITH (  
+        FORMAT_TYPE = DELTA,   
+       ); 
